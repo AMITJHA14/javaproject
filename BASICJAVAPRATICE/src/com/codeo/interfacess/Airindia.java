d9b4003f744453f5f9a7133e06fdea3ad2f85df4
Amitjha14 <amitkj692@gmail.com>
2023-02-16 08:35:08 +0530

    INTERFACES

---
+++ BASICJAVAPRATICE/src/com/codeo/interfacess/Airindia.java
@@ -0,0 +1,11 @@
+package com.codeo.interfacess;
+
+public interface Airindia {
+
+	
+	public void TicketBoooking();
+	public void Ticketcancellation();
+	public void refund();
+	public void SMS();
+	public void TicketBoookingdiscount();
+}
