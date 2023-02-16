bf1043c7939064368c7c6b157ebe4fa52408390f
Amitjha14 <amitkj692@gmail.com>
2023-02-16 08:56:24 +0530

    INTERFACES

---
+++ BASICJAVAPRATICE/src/com/codeo/interfacess/Goibibo.java
@@ -0,0 +1,46 @@
+package com.codeo.interfacess;
+
+public class Goibibo implements Airindia {
+
+	@Override
+	public void TicketBoooking() {
+		System.out.println("Ticket Booking By MakeMyTRIP");
+		
+	}
+	
+	
+
+	@Override
+	public void Ticketcancellation() {
+		System.out.println("Ticket cancellation at MakeMyTRIP");
+		
+	}
+		
+		
+	
+
+	@Override
+	public void refund() {
+		System.out.println("you will refund by Goibibo ");
+
+	}
+
+	@Override
+	public void SMS() {
+		System.out.println("your ticket is booked Successfully");
+		
+		
+	}
+
+
+
+	@Override
+	public void TicketBoookingdiscount() {
+			System.out.println("Get 10% Discount on your first Booking");
+			
+		}
+		
+	}
+
+	
+
